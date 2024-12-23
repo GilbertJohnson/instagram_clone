2ba3abf0737ccb73ce1b43171dbcc0d20415d074
Gilbert Johnson <johnson220898@gmail.com>
2024-12-24 00:55:33 +0530

    Updated AuthPage component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 //import { useState } from 'react'
-import {Button} from './components/ui/button'
+import { Routes, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import AuthPage from "./pages/AuthPage"
+
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button>
-        Click Me!!
-      </Button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,9 @@
-'use client'
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import './index.css';
+import App from './App.jsx';
 import { BrowserRouter } from "react-router-dom";
-
+import { Provider } from "./components/ui/provider";
 
 //import {mode} from '@chakra-ui/theme-tools'
 
@@ -19,19 +16,13 @@ import { BrowserRouter } from "react-router-dom";
 // 	}),
 // }
 
-const conf = {
-  initialColorMode : 'dark',
-  useSystemColorMode : 'false',
-}
-
-const exttheme = extendTheme({conf})   // provide with styles for custom styling
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <ChakraProvider value={exttheme}>
+      <Provider>
         <App/>
-      </ChakraProvider>
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
